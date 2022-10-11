47a6f12e3e159d57cf9fdde6e9ebf47ea1b66f01
Matheus <matheusservulo4@gmail.com>
2022-10-11 09:27:29 -0300

    f5

--- site/src/f5/index.js
+++ site/src/f5/index.js
@@ -1,18 +1,42 @@
 import { useEffect, useState } from "react";
 
 export default function Funcao5(){
+    
+    const [capacidade, setCapacidade] = useState();
+    const [consumo, setConsumo] = useState();
+    const [distancia, setDistancia] = useState();
+    const [total, setTotal] = useState();
+
+    function calcularParadas(capac,consu,dist){
+        let x= capac/consu;
+        let y= dist/x;
+        let final = y/capac;
+        setTotal(Math.trunc(final));
+
+        if(!capac ||!consu ||!dist ){
+            setTotal(0);
+        }
+    }
+
+    useEffect( ()=>{
+        calcularParadas(capacidade,consumo,distancia);
+    },[capacidade,consumo,distancia]);
+
+    
+    
+
     return(
         <section>
             <h1>FUNÇÃO PARADAS </h1>
             <p>CAPACIDADE DO CAMPO EM COMBUSTÍVEL</p>
-            <input type='text'/>
+            <input type='text' value={capacidade} onChange={e=>setCapacidade(e.target.value)}/>
             <p>CONSUMO DO VEICULO</p>
-            <input type='number'/>
+            <input type='number' value={consumo} onChange={e=>setConsumo(e.target.value)}/>
             <p>DISTÂNCIA DA VIAGEM</p>
-            <input type='number'/>
+            <input type='number' value={distancia} onChange={e=>setDistancia(e.target.value)}/>
             <br/>
             <hr/>
-            <span> Você precisará fazer {resultado} para abastecer </span>
+            <span> Você precisará fazer {total} paradas para abastecer </span>
         </section>
     );
 }  
